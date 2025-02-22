7f9debca1e4cdc6809d47429431406d7aadf0e9f
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-22 12:15:59 +0530

    Adding Equity Dictionary

--- Tranding_Simulator.py
+++ Tranding_Simulator.py
@@ -8,8 +8,43 @@ def getStocks(time,equity):
    return response
 
 def main():
-   equity = input("Enter the equity of choice: ")
+   equity_name = input("Enter the equity of choice: ")
    time = input("Enter time series: ")
+   equity_dict = {
+    'Tesla': 'TSLA',
+    'NVIDIA': 'NVDA',
+    'Super Micro Computer': 'SMCI',
+    'Quantum Computing': 'QUBT',
+    'Rigetti Computing': 'RGTI',
+    'MicroStrategy': 'MSTR',
+    'Nukkleus': 'NUKK',
+    'Broadcom': 'AVGO',
+    'CleanSpark': 'CLSK',
+    'Advanced Micro Devices': 'AMD',
+    'Eli Lilly and Company': 'LLY',
+    'SEALSQ Corp': 'LAES',
+    'Micron Technology': 'MU',
+    'D-Wave Quantum': 'QBTS',
+    'Dell Technologies': 'DELL',
+    'Walmart': 'WMT',
+    'Invesco QQQ Trust': 'QQQ',
+    'Apple': 'AAPL',
+    'Amazon.com': 'AMZN',
+    'Palantir Technologies': 'PLTR',
+    'Google': 'GOOGL',
+    'IBM': 'IBM',
+    'Microsoft': 'MSFT',
+    'Meta': 'META'
+   }
+
+   if equity_name in equity_dict.values():
+      equity = equity_name
+   elif equity_name.capitalize() in equity_dict.keys():
+      equity = equity_dict[equity_name.capitalize()]
+   else:
+      print("Data Not available:") 
+      return
+   
    return (getStocks(time.upper(), equity))
 
 if __name__ == "__main__":
